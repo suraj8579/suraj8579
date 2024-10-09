0e7a8cd7b07f8f3f12623ae38f25c393909f8e99
Suraj Kumar Jha <168299107+suraj8579@users.noreply.github.com>
2024-10-09 23:17:42 +0530

    calculator

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+function clearsthemostrecententry(){
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
